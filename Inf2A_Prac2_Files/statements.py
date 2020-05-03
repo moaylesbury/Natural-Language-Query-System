971dde026d72a068609860228d901dc34ac7170e
Michael <moaylesbury@gmail.com>
2020-05-03 14:29:52 +0100

    Finished main part A implementation

--- Inf2A_Prac2_Files/statements.py
+++ Inf2A_Prac2_Files/statements.py
@@ -1,3 +1,6 @@
+import re
+from nltk.corpus import brown
+
 # File: statements.py
 # Template file for Informatics 2A Assignment 2:
 # 'A Natural Language Query System in Python/NLTK'
@@ -10,14 +13,15 @@
 
 # PART A: Processing statements
 
-def add(lst,item):
+def add(lst, item):
     if (item not in lst):
-        lst.insert(len(lst),item)
+        lst.insert(len(lst), item)
+
 
 class Lexicon:
     """stores known word stems of various part-of-speech categories"""
 
-    def __int__(self):
+    def __init__(self):
         self.lb = []   # LexBase
 
     def add(self, stem, cat):
@@ -25,13 +29,12 @@ class Lexicon:
             if ((stem, cat) not in self.lb):
                 self.lb.insert((stem, cat))
             else:
-                print((stem, cat) + " already in lex base")
+                print("(" + stem + ", " + cat + ")" + " already in lex base")
         else:
             print(cat + " is not one of the valid POS categories: P, A, I, N or T")
 
     def getAll(self, cat):
         return [f for f in self.lb if f[1] == cat]
-
 
 
 class FactBase:
@@ -44,13 +47,13 @@ class FactBase:
         if ((pred, e1) not in self.fb):
             self.fb.insert((pred, e1))
         else:
-            print((pred, e1) + " already in fact base")
+            print("(" + pred + ", " + e1 + ")" + " already in fact base")
 
     def addBinary(self, pred, e1, e2):
         if ((pred, e1, e2) not in self.fb):
             self.fb.insert((pred, e1, e2))
         else:
-            print((pred, e1, e2) + " already in fact base")
+            print("(" + pred + ", " + e1 + ", " + e2 + ")" + " already in fact base")
 
     def queryUnary(self, pred, e1):
         return True if ((pred, e1) in self.fb) else False
@@ -58,36 +61,50 @@ class FactBase:
     def queryBinary(self, pred, e1, e2):
         return True if ((pred, e1, e2) in self.fb) else False
 
-import re
-from nltk.corpus import brown 
+
 def verb_stem(s):
     """extracts the stem from the 3sg form of a verb, or returns empty string"""
 
     if (re.match('.*[^aeiousxyz(ch)(sh)]$', s)):                  # ends in s,x,y,z,ch,sh -> add s
         s += 's'
-    elif (re.match('.*[aeiou]y', s)):                             # ends in y preceeded by a vowel -> add s
+        return s
+    elif (re.match('.*[aeiou]y$', s)):                             # ends in y preceded by a vowel -> add s
         s += 's'
-    elif (re.match('.+[^aeiou]y', s)):              # ends in y preceeded by a non vowel and contains at least three letters -> change y to ies
+        return s
+    elif (re.match('.+[^aeiou]y$', s)):              # ends in y preceeded by a non vowel and contains at least three letters -> change y to ies
         s[-1] = 'i'
-        s += 'es'
-    elif (re.match('[^aeiou]ie', s)):                            # form Xie where x is a letter not a vowel -> add s
+        s += "es"
+        return s
+    elif (re.match('[^aeiou]ie$', s)):                            # form Xie where x is a letter not a vowel -> add s
         s += 's'
-    elif (re.match('.*[ox]|.*(ch|sh|ss|zz)', s)):                           # ends in o,x,ch,sh,ss,zz -> add es
-    elif (re.match('.*[^s]se|.*[^z]ze', s)):                        # ends in se or ze but not sse or zze -> add s
+        return s
+    elif (re.match('.+[ox]*(?<=(ch|sh|ss|zz))*$', s)):                           # ends in o,x,ch,sh,ss,zz -> add es
+        s += "es"
+        return s
+    elif (re.match('.+([^s]se|[^z]ze)$', s)):                        # ends in se or ze but not sse or zze -> add s
+        s += 's'
+        return s
     elif (re.match('have', s)):                                 # have -> has
-    elif (re.match('', s)):                                     # ends in e not preceeded by i,o,s,x,z,ch,sh -> add s
+        s[-2] = 's'
+        s = s[:-2]
+        return s
+    elif (re.match('.*[^iosxz](?<!(ch|sh))e$', s)):                                     # ends in e not preceeded by i,o,s,x,z,ch,sh -> add s
+        s += 's'
+        return s
     else:
-        return None
+        return ''
 
-def add_proper_name (w,lx):
+
+def add_proper_name(w, lx):
     """adds a name to a lexicon, checking if first letter is uppercase"""
-    if ('A' <= w[0] and w[0] <= 'Z'):
-        lx.add(w,'P')
+    if ('A' <= w[0] <= 'Z'):
+        lx.add(w, 'P')
         return ''
     else:
         return (w + " isn't a proper name")
 
-def process_statement (lx,wlist,fb):
+
+def process_statement(lx, wlist, fb):
     """analyses a statement and updates lexicon and fact base accordingly;
        returns '' if successful, or error message if not."""
     # Grammar for the statement language is:
@@ -97,23 +114,23 @@ def process_statement (lx,wlist,fb):
     msg = add_proper_name (wlist[0],lx)
     if (msg == ''):
         if (wlist[1] == 'is'):
-            if (wlist[2] in ['a','an']):
-                lx.add (wlist[3],'N')
-                fb.addUnary ('N_'+wlist[3],wlist[0])
+            if (wlist[2] in ['a', 'an']):
+                lx.add(wlist[3],'N')
+                fb.addUnary('N_'+wlist[3],wlist[0])
             else:
-                lx.add (wlist[2],'A')
-                fb.addUnary ('A_'+wlist[2],wlist[0])
+                lx.add(wlist[2],'A')
+                fb.addUnary('A_'+wlist[2],wlist[0])
         else:
             stem = verb_stem(wlist[1])
             if (len(wlist) == 2):
-                lx.add (stem,'I')
-                fb.addUnary ('I_'+stem,wlist[0])
+                lx.add(stem, 'I')
+                fb.addUnary('I_'+stem, wlist[0])
             else:
-                msg = add_proper_name (wlist[2],lx)
+                msg = add_proper_name(wlist[2],lx)
                 if (msg == ''):
-                    lx.add (stem,'T')
-                    fb.addBinary ('T_'+stem,wlist[0],wlist[2])
+                    lx.add(stem, 'T')
+                    fb.addBinary('T_'+stem, wlist[0], wlist[2])
     return msg
-                        
+
 # End of PART A.
 
